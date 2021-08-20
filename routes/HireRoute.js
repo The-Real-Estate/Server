48d1c884176db439782192a1fa8d81e22f63ca12
bishal76 <budhabishal280@gmail.com>
2021-08-20 16:02:43 +0545

    this is login

--- routes/HireRoute.js
+++ routes/HireRoute.js
@@ -1,68 +1,68 @@
-const express = require('express');
-const { check, validationResult } = require('express-validator');
-const bcryptjs = require('bcryptjs');
-const Hire = require('../models/HireModel');
-const jwt = require('jsonwebtoken');
-const router = express.Router();
-const auth = require('../Middleware/Authenticate')
+// const express = require('express');
+// const { check, validationResult } = require('express-validator');
+// const bcryptjs = require('bcryptjs');
+// const Hire = require('../models/HireModel');
+// const jwt = require('jsonwebtoken');
+// const router = express.Router();
+// const auth = require('../Middleware/Authenticate')
 
-router.post('/hire/insert', function (req, res) {
-    console.log(req.body)
+// router.post('/hire/insert', function (req, res) {
+//     console.log(req.body)
    
     
-        //valid
-        const HireName = req.body.HireName;
-        const HirePhone = req.body.HirePhone;
-        const ArtistName = req.body.ArtistName;
-        const ArtistPhone = req.body.ArtistPhone;
-           // console.log(us);
-        // console.log(add); 
+//         //valid
+//         const HireName = req.body.HireName;
+//         const HirePhone = req.body.HirePhone;
+//         const ArtistName = req.body.ArtistName;
+//         const ArtistPhone = req.body.ArtistPhone;
+//            // console.log(us);
+//         // console.log(add); 
         
-            const data = new Hire({
-                HireName: HireName,
-                HirePhone: HirePhone,
-                ArtistName: ArtistName,
-                ArtistPhone: ArtistPhone,
+//             const data = new Hire({
+//                 HireName: HireName,
+//                 HirePhone: HirePhone,
+//                 ArtistName: ArtistName,
+//                 ArtistPhone: ArtistPhone,
              
-            });
+//             });
 
-            data.save()
+//             data.save()
             
-            .then(function (result) {
+//             .then(function (result) {
                 
-                console.log(data)
-                return res.status(201).json({ success:true,message: "Registration success !!!!" })
-            })// sucessess vayo ki vaena
-            .catch(function (err45) {
-                console.log(err45)
-                res.status(500).json({ error: err45 })
-            })// error aayo ki aayena
-        })
+//                 console.log(data)
+//                 return res.status(201).json({ success:true,message: "Registration success !!!!" })
+//             })// sucessess vayo ki vaena
+//             .catch(function (err45) {
+//                 console.log(err45)
+//                 res.status(500).json({ error: err45 })
+//             })// error aayo ki aayena
+//         })
         
-router.get('/register_show', function (req, res) {
-    // console.log("this is for showing data")
-    // res.send("test show")
-    Hire.find().then(function (data) {
-        // console.log(data);
-        res.send(data);
-    })
-})
+// router.get('/register_show', function (req, res) {
+//     // console.log("this is for showing data")
+//     // res.send("test show")
+//     Hire.find().then(function (data) {
+//         // console.log(data);
+//         res.send(data);
+//     })
+// })
 
-// for delete
-router.delete('/register_delete/:id', auth.verifyUser, function (req, res) {
-    //delete code
-    const id = req.params.id;
-    Hire.deleteOne({ _id: id }).then(function () {
-        res.send("Deleted !")
-    })
+// // for delete
+// router.delete('/register_delete/:id', auth.verifyUser, function (req, res) {
+//     //delete code
+//     const id = req.params.id;
+//     Hire.deleteOne({ _id: id }).then(function () {
+//         res.send("Deleted !")
+//     })
 
-})
-// for update
-router.put('/register_update/:id', function (req, res) {
-    const id = req.params.id;
-    const book_name = req.body.book_name;
-    Hire.updateOne({ _id: id }, { Email: Email }).then(function () {
-        res.send("Updated!")
-    })
-})
-module.exports = router;
+// })
+// // for update
+// router.put('/register_update/:id', function (req, res) {
+//     const id = req.params.id;
+//     const book_name = req.body.book_name;
+//     Hire.updateOne({ _id: id }, { Email: Email }).then(function () {
+//         res.send("Updated!")
+//     })
+// })
+// module.exports = router;
